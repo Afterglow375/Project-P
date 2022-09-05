449aa9b37a05c00753abe5d23059327fc9385782
Alex Tatusko <afterglow376@gmail.com>
2022-09-05 15:08:21 -0400

    Fixed prefabs

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class BallController : MonoBehaviour
 {
     int points = 0;
     public int force = 50;
