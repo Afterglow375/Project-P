74cdbf7902ea9eacd7cd0d7967a0be84c804d065
Alex Tatusko <afterglow376@gmail.com>
2022-09-07 14:42:12 -0400

    [20] Setup player and enemy hp to take damage after shots

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,8 +7,10 @@ public class BallController : MonoBehaviour
     public int force = 50;
     public bool isLaunched = false;
     public GameObject trail;
+    public int minEnemyDamage = 0;
+    public int maxEnemyDamage = 30;
     
-    private int _points;
+    private int _pegScore;
     private int _pegCount;
     private Rigidbody2D _body;
     private Vector2 _startPos;
@@ -35,12 +37,12 @@ public class BallController : MonoBehaviour
             _pegCount++;
             if (_pegCount == 5)
             {
-                _points += 10;
+                _pegScore += 10;
                 _pegCount = 0;
             }
             
-            _points += peg.points;
-            _combatHUD.UpdatePegScoreText(_points);
+            _pegScore += peg.points;
+            _combatHUD.UpdatePegScoreText(_pegScore);
         }
     }
 
@@ -56,13 +58,13 @@ public class BallController : MonoBehaviour
     {
         if (!isLaunched)
         {
-            _combatHUD.UpdatePegScoreText();
-            _points = 0;
-            _pegCount = 0;
             _body.simulated = true;
             _body.constraints = RigidbodyConstraints2D.None;
             _body.AddForce(shootDirection.normalized * force);
             isLaunched = true;
+            _combatHUD.UpdatePegScoreText();
+            _pegScore = 0;
+            _pegCount = 0;
         }
     }
     private void ResetPos()
@@ -72,5 +74,8 @@ public class BallController : MonoBehaviour
         _body.simulated = false;
         isLaunched = false;
         _trailRenderer.Clear();
+        int enemyDamage = Random.Range(minEnemyDamage, maxEnemyDamage+1);
+        _combatHUD.UpdatePlayerHealthText(-enemyDamage);
+        _combatHUD.UpdateEnemyHealthText(-_pegScore);
     }
 }
