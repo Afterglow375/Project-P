706734a404893af10b891843859670b1e31b15d8
Alex Tatusko <afterglow376@gmail.com>
2022-09-08 00:43:20 -0400

    [36] Added functionality to main menu UI

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string level;
+    
+    // these functions are connected to the respective main menu buttons
+    public void Play()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LevelSelect()
+    {
+        // TODO: switch scene to level select
+        Debug.Log("level select!");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("quitting!");
+    }
+}
