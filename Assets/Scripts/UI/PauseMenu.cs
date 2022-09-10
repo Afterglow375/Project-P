6b5e7bc4f1f5f12b0d47be70616b339dcfa6c260
Isak <shoeboss69@gmail.com>
2022-09-10 23:50:10 +0200

    Fix pause

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -48,6 +48,7 @@ public class PauseMenu : MonoBehaviour
     public void MainMenu()
     {
         Time.timeScale = 1f;
+        isPaused = false;
         SceneManager.LoadScene("Main Menu");
     }
 }
