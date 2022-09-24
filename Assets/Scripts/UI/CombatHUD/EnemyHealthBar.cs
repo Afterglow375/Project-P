bb9146a0db78dc002082a62b6055b788ae9d0fb4
Alex Tatusko <afterglow376@gmail.com>
2022-09-24 13:10:02 -0400

    [68] Added health bars to combat area

---
+++ Assets/Scripts/UI/CombatHUD/EnemyHealthBar.cs
@@ -0,0 +1,33 @@
+using System;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.CombatHUD
+{
+    public class EnemyHealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image _healthBarSprite;
+        private int _maxHealth;
+        
+        private void Awake()
+        {
+            CombatManager.EnemyHealthChangeEvent += UpdateHealthBar;
+        }
+
+        private void Start()
+        {
+            _maxHealth = CombatManager.Instance.GetMaxEnemyHp();
+        }
+
+        private void OnDestroy()
+        {
+            CombatManager.EnemyHealthChangeEvent -= UpdateHealthBar;
+        }
+
+        public void UpdateHealthBar(int currentHealth, int damage)
+        {
+            _healthBarSprite.fillAmount = (float) currentHealth / _maxHealth;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/CombatHUD/PlayerHealthBar.cs
@@ -0,0 +1,33 @@
+using System;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.CombatHUD
+{
+    public class PlayerHealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image _healthBarSprite;
+        private int _maxHealth;
+        
+        private void Awake()
+        {
+            CombatManager.PlayerHealthChangeEvent += UpdateHealthBar;
+        }
+
+        private void Start()
+        {
+            _maxHealth = CombatManager.Instance.GetMaxPlayerHp();
+        }
+
+        private void OnDestroy()
+        {
+            CombatManager.PlayerHealthChangeEvent -= UpdateHealthBar;
+        }
+
+        public void UpdateHealthBar(int currentHealth, int damage)
+        {
+            _healthBarSprite.fillAmount = (float) currentHealth / _maxHealth;
+        }
+    }
+}
