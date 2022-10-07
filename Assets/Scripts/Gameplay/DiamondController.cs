ad87089cd3a6f4922433d2ab7d5eeecc21013bdc
Alex Tatusko <afterglow376@gmail.com>
2022-10-07 12:43:26 -0400

    [65] Added Diamond component

---
+++ Assets/Scripts/Gameplay/DiamondController.cs
@@ -0,0 +1,22 @@
+using System;
+using Managers;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class DiamondController : MonoBehaviour
+    {
+        [SerializeField] private int _points;
+        public static event Action<int> DiamondHitEvent;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Ball"))
+            {
+                gameObject.SetActive(false);
+                CombatManager.Instance.SpawnDamageNumber(_points, transform);
+                DiamondHitEvent?.Invoke(_points);
+            }
+        }
+    }
+}
